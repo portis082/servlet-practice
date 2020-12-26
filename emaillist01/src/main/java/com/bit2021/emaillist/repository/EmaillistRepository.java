dc41c1fe7527393b0107c8d958631bdbfb0f5d9e
MS <MS@DESKTOP-VEVTDE0>
2020-12-26 23:12:09 +0900

    repository

--- emaillist01/src/main/java/com/bit2021/emaillist/repository/EmaillistRepository.java
+++ emaillist01/src/main/java/com/bit2021/emaillist/repository/EmaillistRepository.java
@@ -1,5 +1,32 @@
 package com.bit2021.emaillist.repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import com.bit2021.emaillist.vo.EmaillistVo;
+
 public class EmaillistRepository {
+	public boolean insert(EmaillistVo vo) {
+		boolean result = false;
+		Connection connection = null;
+		Statement stmt = null;
+		
+		try {
+			// JDBC Driver
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			// connection
+			String url = "";
+			connection = DriverManager.getConnection(url, "", "");
+			
+			// statement 객체 생성
+			stmt = connection.createStatement();
+			
+			// sql 실행
+			String sql =
+					"";
+		}
+	}
 
 }
